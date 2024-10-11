10f320d48b4c265bd77217abc968b618bda7e6ef
Francesc Brau <frjosepgbrau@gmail.com>
2024-10-11 14:17:23 +0400

    first_comit

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,51 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const Task1App());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Task1App extends StatelessWidget {
+  const Task1App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Steckbrief Lama',
+      theme: ThemeData(fontFamily: 'Lato'),
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+  @override
+  State<MainScreen> createState() => MainScreenState();
+}
+
+class MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Steckbrief Lama',
+          style: TextStyle(color: Colors.white70),
         ),
+        backgroundColor: const Color.fromARGB(225, 143, 186, 211),
       ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Image.asset(
+            'assets/images/lama.png',
+            width: 200,
+            height: 300,
+          ),
+          const SizedBox(height: 20),
+        ],
+      )),
     );
   }
 }
